f30b6adebae908c285075ab142f6e045578b15ad
Erick Cruz <erickcruz147@gmail.com>
2026-03-23 19:48:44 -0500

    replace base with github name

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,24 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import federation from '@originjs/vite-plugin-federation'
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import federation from "@originjs/vite-plugin-federation";
 
 export default defineConfig({
   plugins: [
     vue(),
     federation({
-      name: 'mfe_vue_tasks',
-      filename: 'remoteEntry.js',
+      name: "mfe_vue_tasks",
+      filename: "remoteEntry.js",
       exposes: {
-        './TaskWidget': './src/TaskWidget.vue',
+        "./TaskWidget": "./src/TaskWidget.vue",
       },
-      shared: ['vue'],
+      shared: ["vue"],
     }),
   ],
   build: {
-    target: 'esnext',
+    target: "esnext",
     minify: false,
     cssCodeSplit: false,
   },
   // Change 'erickcguz' to your actual GitHub username
-  base: 'https://erickcguz.github.io/mfe-vue-tasks/dist/',
-})
+  base: "https://hoizard.github.io/mfe-vue-tasks/dist/",
+});
